576e0bb3fc33155aa25fac28f65ce678cdbd300e
Lieven <lieven@mac.home>
2026-06-17 15:19:15 +0200

    preprocessing app

---
+++ R/launchMsqrob2PreprocessingApp.R
@@ -0,0 +1,21 @@
+#' Launches the msqrob2 Shiny App
+#'
+#' @param maxSize maximum memory size that input files are allowed to have in Mb
+#'
+#' @export launchMsqrob2PreprocessingApp
+#'
+#' @return shiny application object
+#'
+#' @example
+#' \dontrun{launchMsqrob2ModelingApp()}
+#'
+#' @import shiny shinymeta shinyjs msqrob2 QFeatures DT SummarizedExperiment shinybusy
+#'
+
+
+# wrapper for shiny::shinyApp()
+launchMsqrob2PreprocessingApp <- function(maxSize=500) {
+  shinyjs::useShinyjs()
+  options(shiny.maxRequestSize=maxSize*1024^2)
+  shinyApp(ui = msqrob2PreprocessingUI, server = msqrob2PreprocessingServer)
+}
---
+++ R/msqrob2PreprocessingServer.R
@@ -0,0 +1,34 @@
+#' Shiny app server function
+#'
+#' @param input provided by shiny
+#' @param output provided by shiny
+#' @param session provided by shiny
+#' @importFrom shinyjs useShinyjs
+#' @importFrom shiny reactiveValues
+
+
+# Define server
+msqrob2PreprocessingServer <- function(input, output, session) {
+  shinyjs::useShinyjs()
+
+  ### servers
+   ### input
+  shinyjs::useShinyjs()
+
+  global_variables <- reactiveValues(pe = NULL,
+                                     selectedAssay = NULL,
+                                     selectedLowLevelAssay = NULL) #Has to be done globally
+
+  importServerInput <- importServer(variables = global_variables)
+  
+  preprocessingServerInput <- preprocessingServer(variables = global_variables)
+  
+  qcServerInput <- qcServer(variables = global_variables)
+
+  #reportServerInput <- reportPreprocessingServer()
+
+  ############################################################
+  #Stop the App when closing the browser or ending the session
+  ############################################################
+  session$onSessionEnded(stopApp)
+}
